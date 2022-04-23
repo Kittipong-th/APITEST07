f4a280c0eff5a12a4175f0efed6330d7b79856e7
Kittipong6306021630013 <6306021630013@fitm.kmutnb.ac.th>
2022-04-23 20:52:40 +0700

    commit

--- models/user.js
+++ models/user.js
@@ -1,20 +1,19 @@
+
 const mongoose = require('mongoose')
 
+
 const userSchema = new mongoose.Schema({
-  userID: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  subscribeDate: {
-    type: Date,
-    required: true,
-    default: Date.now
-  }
-})
+    userID: String,
+    password: String,
+    name:String,
+    room:Number,
+    elec_use:Number,
+    roomate:Number,
+    Status:false,
+    image : String
+        
+  })
+
 //สร้างโมเดล
 let Users = mongoose.model("User",userSchema)
 
