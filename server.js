6bc4cd27bdfc4c9f0f077c3448f0b6a0f0548cea
Kittipong6306021630013 <6306021630013@fitm.kmutnb.ac.th>
2022-04-23 23:46:33 +0700

    commit

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express()
 const mongoose = require('mongoose')
 
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL,{
+  useNewUrlParser : true,
+  useUnifiedTopology : true
+})
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open',() => console.log('Connected to Database'))
