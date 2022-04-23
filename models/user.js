a1cf32b0b8559d54a516123f84bfe09c081d3029
Kittipong6306021630013 <6306021630013@fitm.kmutnb.ac.th>
2022-04-24 00:05:22 +0700

    commit

--- models/user.js
+++ models/user.js
@@ -8,12 +8,8 @@ const subscriberSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true
-  },
-  subscribeDate: {
-    type: Date,
-    required: true,
-    default: Date.now
   }
+  
 })
 
 module.exports = mongoose.model('User', subscriberSchema)
